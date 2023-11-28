83682326435a69d60f5dffecb1f7c7d8879cc76f
sp1rale <dimayaremchyk24@gmail.com>
2023-11-28 20:29:42 +0200

    done

--- Header.h
+++ Header.h
@@ -1,1 +1,65 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+struct Fraction {
+    int numerator;
+    int denominator;
+
+    void simplify() {
+        int gcd = findGCD(abs(numerator), abs(denominator));
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    void toProper() {
+        if (numerator >= denominator) {
+            int wholePart = numerator / denominator;
+            numerator %= denominator;
+            cout << "Whole part: " << wholePart << ", ";
+        }
+    }
+};
+
+Fraction addFractions(const Fraction& frac1, const Fraction& frac2) {
+    Fraction result;
+    result.numerator = frac1.numerator * frac2.denominator + frac2.numerator * frac1.denominator;
+    result.denominator = frac1.denominator * frac2.denominator;
+    result.simplify();
+    return result;
+}
+
+Fraction subtractFractions(const Fraction& frac1, const Fraction& frac2) {
+    Fraction result;
+    result.numerator = frac1.numerator * frac2.denominator - frac2.numerator * frac1.denominator;
+    result.denominator = frac1.denominator * frac2.denominator;
+    result.simplify();
+    return result;
+}
+
+
+Fraction multiplyFractions(const Fraction& frac1, const Fraction& frac2) {
+    Fraction result;
+    result.numerator = frac1.numerator * frac2.numerator;
+    result.denominator = frac1.denominator * frac2.denominator;
+    result.simplify();
+    return result;
+}
+
+Fraction divideFractions(const Fraction& frac1, const Fraction& frac2) {
+    Fraction result;
+    result.numerator = frac1.numerator * frac2.denominator;
+    result.denominator = frac1.denominator * frac2.numerator;
+    result.simplify();
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
-#include <iostream>
+#include "Header.h"
 
-using namespace std;
+int main() {
+    Fraction frac1, frac2;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	return 0;
+    cout << "Enter the numerator of the first fraction: ";
+    cin >> frac1.numerator;
+
+    cout << "Enter the denominator of the first fraction (not equal to zero): ";
+    cin >> frac1.denominator;
+
+    cout << "Enter the numerator of the second fraction: ";
+    cin >> frac2.numerator;
+
+    cout << "Enter the denominator of the second fraction (not equal to zero): ";
+    cin >> frac2.denominator;
+
+    cout << "First fraction: " << frac1.numerator << "/" << frac1.denominator << endl;
+    cout << "Second fraction: " << frac2.numerator << "/" << frac2.denominator << endl;
+
+    Fraction sum = addFractions(frac1, frac2);
+    sum.toProper();
+    cout << "Sum: " << sum.numerator << "/" << sum.denominator << endl;
+
+    Fraction difference = subtractFractions(frac1, frac2);
+    difference.toProper();
+    cout << "Difference: " << difference.numerator << "/" << difference.denominator << endl;
+
+    Fraction product = multiplyFractions(frac1, frac2);
+    product.toProper();
+    cout << "Product: " << product.numerator << "/" << product.denominator << endl;
+
+    Fraction quotient = divideFractions(frac1, frac2);
+    quotient.toProper();
+    cout << "Quotient: " << quotient.numerator << "/" << quotient.denominator << endl;
+
+    return 0;
 }
